f057bdeb115422b456b25ef5e7603b06dc8e66e8
mitsuru_yamaura <komayou326@gmail.com>
2021-06-24 16:08:19 +0900

    GimmickBase.cs 作成。行動用のボタン操作時に Ray によりギミック感知処理の実装。ギミックを自動的に破壊するか、そのまま残すか分岐処理実装。

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -13,6 +13,15 @@ public class PlayerController : MonoBehaviour
     [SerializeField]
     private float moveSpeed;
 
+    [SerializeField]
+    private float searchRange;
+
+    [SerializeField]
+    private LayerMask layerMask;
+
+    [SerializeField]
+    private Transform searchTran;
+
     void Start()
     {
         TryGetComponent(out rb);
@@ -23,8 +32,12 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         MoveInput();
+        SearchAction();
     }
 
+    /// <summary>
+    /// ÉLÅ[ì¸óÕ
+    /// </summary>
     private void MoveInput() {
         x = Input.GetAxis("Horizontal");
         z = Input.GetAxis("Vertical");
@@ -34,7 +47,29 @@ public class PlayerController : MonoBehaviour
         Move();    
     }
 
+    /// <summary>
+    /// à⁄ìÆ
+    /// </summary>
     private void Move() {
         rb.velocity = new Vector3(x * moveSpeed, rb.velocity.y, z * moveSpeed);
+    }
+
+    /// <summary>
+    /// íTçı
+    /// </summary>
+    private void SearchAction() {
+        if (Input.GetButtonDown("Action")) {
+            Ray ray = new Ray(searchTran.position, searchTran.forward);
+            Debug.DrawRay(ray.origin, ray.direction, Color.red, 1.0f);
+
+            if (Physics.Raycast(ray, out RaycastHit hit, searchRange, layerMask)) {
+
+                Debug.Log(hit.collider.gameObject.name);
+
+                if (hit.collider.TryGetComponent(out GimmickBase gimmic)) {
+                    gimmic.TriggerGimmick();
+                }
+            }
+        }
     }
 }
