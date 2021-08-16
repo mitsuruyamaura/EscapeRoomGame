0f875ca0020b4d34ec18593c7abb3119895fb0c1
mitsuru_yamaura <komayou326@gmail.com>
2021-08-16 11:43:28 +0900

    クリアに必要なアイテムのみを所持している場合にゲームクリアになる機能の実装。デバッグ開始。

--- Assets/Scripts/Items/ClearChecker.cs
+++ Assets/Scripts/Items/ClearChecker.cs
@@ -4,21 +4,24 @@ using UnityEngine;
 
 public class ClearChecker : MonoBehaviour
 {
-    [SerializeField, Header("クリアに必要なアイテム")]
-    private bool[] needClearItems;
-
     [SerializeField, Header("クリアに必要なアイテムの名前")]
-    private ItemType[] needClearItemNames;
+    private ItemType[] needClearItemTypes;
 
 
     private void OnTriggerEnter(Collider other) {
         if (other.gameObject.TryGetComponent(out PlayerController player)) {
-            JudgeGameClearAllItems(player.itemManager);
+
+            // ゲームクリア判定の方法の分岐
+            if (needClearItemTypes.Length > 0) {
+                JudgeGameClearItemTypes(player.itemManager);
+            } else {
+                JudgeGameClearAllItems(player.itemManager);
+            }            
         }
     }
 
     /// <summary>
-    /// 所持アイテムの確認をしてゲームクリアの判定
+    /// すべての所持アイテムの確認をしてゲームクリアの判定
     /// </summary>
     public void JudgeGameClearAllItems(ItemManager itemManager) {
 
@@ -26,6 +29,7 @@ public class ClearChecker : MonoBehaviour
             if (itemManager.haveItems[i]) {
                 continue;
             } else {
+                Debug.Log("アイテムが足りない");
                 return;
             }
         }
@@ -33,5 +37,22 @@ public class ClearChecker : MonoBehaviour
         Debug.Log("ゲームクリア");
     }
 
+    /// <summary>
+    /// クリアに必要なアイテムのみを確認してゲームクリアの判定
+    /// </summary>
+    /// <param name="itemManager"></param>
+    public void JudgeGameClearItemTypes(ItemManager itemManager) {
+        foreach (ItemType itemType in needClearItemTypes) {
+            for (int i = 0; i < itemManager.haveItems.Length; i++) {
+                if (itemManager.haveItems[(int)itemType]) {
+                    continue;
+                } else {
+                    Debug.Log("アイテムが足りない");
+                    return;
+                }
+            }
+        }
 
+        Debug.Log("ゲームクリア");
+    }
 }
